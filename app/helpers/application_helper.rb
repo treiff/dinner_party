c49195463a387339c51b248d434736eb844ba9e4
Trevor Reiff <trevorreiff@gmail.com>
2015-03-05 20:26:33 -0500

    Implemented skeleton static pages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Helper method to display full title on each page
+  def full_title(page_title)
+    base_title = "Dinner Party"
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
