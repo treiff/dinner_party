2c6e0dc71e81e359678600d929a3cf38321b6eeb
Trevor Reiff <trevorreiff@gmail.com>
2015-03-06 10:07:24 -0500

    Change routes static pages for v1

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -1,31 +1,12 @@
 require 'rails_helper'
 
-describe 'static pages' do
+describe 'create dinner party' do
   describe 'home page' do
     before { visit root_path }
     it 'has a valid title' do
       expect(page).to have_title('Dinner Party | Home')
     end
   end
-
-  describe 'about page' do
-    before { visit about_path }
-    it 'has a valid title' do
-      expect(page).to have_title('Dinner Party | About')
-    end
-  end
-
-  describe 'contact page' do
-    before { visit contact_path }
-    it 'has a valid title' do
-      expect(page).to have_title('Dinner Party | Contact')
-    end
-  end
-
-  describe 'help page' do
-    before { visit help_path }
-    it 'has a valid title' do
-      expect(page).to have_title('Dinner Party | Help')
-    end
-  end
 end
+
+# make more of a use case.
