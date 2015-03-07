f79277be74f3614a79b2cc77e0c2516d509dcd91
Trevor Reiff <trevorreiff@gmail.com>
2015-03-07 13:50:08 -0500

    Basic edit form

--- app/models/dinner.rb
+++ app/models/dinner.rb
@@ -1,20 +1,24 @@
 class Dinner < ActiveRecord::Base
   validates :email, email: true, case_sensitive: false
   validates :email, :dinner_name, :date, :time, :location, presence: true
+  attr_readonly :email, :dinner_name, :date, :time, :location
 
-  before_save :generate_unique_url
+  before_create :generate_unique_url
 
   def custom_url
-    "http://localhost:3000/dinners/#{self.unique_url}"
+    "http://localhost:3000/dinners/#{self.unique_url}/edit"
+  end
+
+  # Overides rails default to_param
+  # When params called on a dinner object unique_url
+  # replaces :id
+  def to_param
+    unique_url
   end
 
   private
 
   def generate_unique_url
     self.unique_url = SecureRandom.hex
-  end
-
-  def to_param
-    unique_url
   end
 end
