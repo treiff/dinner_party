48454c7a40e82ba028e51a7d968fbb368c2f1da8
Trevor Reiff <trevorreiff@gmail.com>
2015-03-08 13:56:28 -0400

    Small refactor of dinner table

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150308003811) do
+ActiveRecord::Schema.define(version: 20150308173859) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "dinners", force: :cascade do |t|
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "dinner_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name"
     t.date     "date"
     t.time     "time"
     t.string   "location"
