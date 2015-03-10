0edfedad02d825479ca0dba6f386503b7d3a03f5
Trevor Reiff <trevorreiff@gmail.com>
2015-03-10 12:29:05 -0400

    Fixed some flash error issues

--- app/controllers/dinners_controller.rb
+++ app/controllers/dinners_controller.rb
@@ -6,10 +6,8 @@ class DinnersController < ApplicationController
   def create
     @dinner = Dinner.new(dinner_params)
     if @dinner.save
-      flash[:notice] = "Here's your link copy and send to friends: #{@dinner.custom_url}"
-      redirect_to root_path
+      redirect_to root_path, notice: "Here's your link copy and send to friends: #{@dinner.custom_url}"
     else
-      flash.now[:error] = "Please complete all fields"
       render 'new'
     end
   end
@@ -21,10 +19,8 @@ class DinnersController < ApplicationController
   def update
     @dinner = Dinner.find_by_unique_url(params[:id])
     if @dinner.update_attributes(dinner_params)
-      flash[:notice] = "Great, see you there!"
-      redirect_to root_path
+      redirect_to root_path, notice: "Great, see you there!"
     else
-      flash.now[:error] = @dinner.errors.full_messages.to_sentence
       render 'edit'
     end
   end
