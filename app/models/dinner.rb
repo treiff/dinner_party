961a7bd1492a45d6fd00d640e57031fd9be700fb
Trevor Reiff <trevorreiff@gmail.com>
2015-03-13 13:56:41 -0400

    Extract URL into helper module

--- app/models/dinner.rb
+++ app/models/dinner.rb
@@ -1,5 +1,7 @@
 # Generates dinner objects
 class Dinner < ActiveRecord::Base
+  include Url
+
   validates :email, email: true, case_sensitive: false, presence: true
   validates :name, presence: true
   validates :date, presence: true
@@ -10,20 +12,10 @@ class Dinner < ActiveRecord::Base
 
   before_create :generate_unique_url
 
-  def custom_url
-    "http://majormunch.herokuapp.com/dinners/#{unique_url}"
-  end
-
   # Overides rails default to_param
   # When params called on a dinner object unique_url
   # replaces :id
   def to_param
     unique_url
-  end
-
-  private
-
-  def generate_unique_url
-    self.unique_url = SecureRandom.hex
   end
 end
