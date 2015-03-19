afbb360178517d734b194df44a220038b615df20
Trevor Reiff <trevorreiff@gmail.com>
2015-03-19 10:32:49 -0400

    Implement RSVP deletion

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   resources :dinners, only: [:new, :create, :show] do
-    resources :rsvps, only: [:create, :edit, :update]
+    resources :rsvps, only: [:create, :edit, :update, :destroy]
   end
 
   root to: "dinners#new"
