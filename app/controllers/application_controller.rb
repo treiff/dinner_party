e18d4154a52413e987d36acd4b338e767b32e113
Trevor Reiff <trevorreiff@gmail.com>
2015-03-19 11:49:45 -0400

    Added cookie store for rsvp creator

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_rsvp_user
 
+  def current_rsvp_user
+    current_rsvp_user ||= cookies[:current_rsvp_id] && Rsvp.find_by(id: cookies[:current_rsvp_id])
+  end
 end
--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,8 +1,9 @@
 class RsvpsController < ApplicationController
   def create
     dinner = Dinner.find_by_slug(params[:dinner_id])
-    @rsvp = Rsvp.new(rsvp_params)
-    if @rsvp.save
+    rsvp = Rsvp.new(rsvp_params)
+    if rsvp.save
+      cookies[:current_rsvp_id] = rsvp.id
       redirect_to dinner_path(dinner), notice: "RSVP'ed, Thanks!"
     else
       redirect_to root_path, notice: "Sorry, make sure all fields are filled."
@@ -20,9 +21,9 @@ class RsvpsController < ApplicationController
 
   def update
     dinner = Dinner.find_by_slug(params[:dinner_id])
-    @rsvp = dinner.rsvps.find(params[:id])
+    rsvp = dinner.rsvps.find(params[:id])
 
-    if @rsvp.update_attributes(rsvp_params)
+    if rsvp.update_attributes(rsvp_params)
       redirect_to dinner_path(dinner), notice: "RSVP updated successfully"
     else
       redirect_to root_path, notice: "Something went wrong"
@@ -32,6 +33,7 @@ class RsvpsController < ApplicationController
   def destroy
     dinner = Dinner.find_by_slug(params[:dinner_id])
     dinner.rsvps.find(params[:id]).destroy
+    cookies.delete :current_rsvp_id
     flash[:success] = "RSVP deleted"
     redirect_to dinner_path(dinner)
   end
